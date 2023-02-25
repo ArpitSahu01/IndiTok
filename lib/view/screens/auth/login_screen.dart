913f0f12c3ae1b15fe2928ca7be528cc82cc02b2
arpit <arpitsahu789@gmail.com>
2023-02-25 12:27:09 +0530

    added login screeen

---
+++ lib/view/screens/auth/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:inditok/view/widgets/glitch.dart';
+import 'package:inditok/view/widgets/text_input.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  LoginScreen({Key? key}) : super(key: key);
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GlithEffect(child: const Text("INDITOK",style: TextStyle(fontWeight: FontWeight.w900,fontSize: 40),)),
+            const SizedBox(height: 30,),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInput(controller: _emailController, myIcon: Icons.email, myLabelText: "Email"),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInput(controller: _passwordController, myIcon: Icons.password, myLabelText: "Password",toHide: true,),
+            ),
+            const SizedBox(height: 30,),
+           ElevatedButton(onPressed: (){}, child: Container(
+               padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+               child: const Text("LOGIN",style: TextStyle(fontSize: 15),),),),
+          ],
+        ),
+      ),
+    );
+  }
+}
