1369b43fe9252381ee961a7a9e68ba9584d23e21
arpit <arpitsahu789@gmail.com>
2023-02-25 15:22:40 +0530

    imported packages to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:inditok/constants.dart';
 import 'package:inditok/view/screens/auth/login_screen.dart';
 import 'package:inditok/view/screens/auth/signup_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
